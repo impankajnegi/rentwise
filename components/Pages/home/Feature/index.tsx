72883dbc5cde29ba024bafd37e7d20a3007da5a1
Pankaj Negi <pnegi.dun@gmail.com>
2024-10-09 01:13:18 +0530

    added featurea and filter com to home page

--- components/Pages/home/Feature/index.tsx
+++ components/Pages/home/Feature/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Index = () => {
-    return (
-        <div>
-            feture
+const Feature = () => {
+  return (
+    <div>
+      <div className=" flex p-2">
+        <div className="flex">
+          <h1 className="text-5xl font-extrabold dark:text-white">
+            Jackets
+            <small className="ms-2 font-semibold text-gray-500 dark:text-gray-400">
+              on Rentwise!
+            </small>
+          </h1>
         </div>
-    );
-}
+        <div className="flex"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Index;
+export default Feature;
--- components/Pages/home/index.tsx
+++ components/Pages/home/index.tsx
@@ -5,23 +5,30 @@ import Breadcrumb from "../Breadcrumb";
 import ItemList from "@/components/HotDealsSlider";
 import CategoryList from "@/components/CategoryList";
 import MostBoughtSlider from "@/components/MostBoughtSlider/intex";
+import Feature from "@/components/Pages/home/Feature";
+import Filters from "@/components/Pages/home/Filters";
 // import axios from 'axios';
 
 export default function ImageSliderWithSidebar() {
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <Sidebar></Sidebar>
-      {/* Image Slider */}
+    <>
+      <div className="flex-col mt-24 p-4 justify-start mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <Breadcrumb></Breadcrumb>
+        <Feature></Feature>
+        <Filters></Filters>
+        <ItemList></ItemList>
+
+        {/* <Sidebar></Sidebar> 
       <div className="w-full relative">
         <Banner></Banner>
         <div className="h-4/5">
-        {/* <Breadcrumb></Breadcrumb> */}
-        <CategoryList></CategoryList>
-        <ItemList listHeader="Hot Deals"></ItemList>
-        <MostBoughtSlider listHeader="Most Boughts on RentWise!"></MostBoughtSlider> 
+          <Breadcrumb></Breadcrumb>
+          <CategoryList></CategoryList>
+          <ItemList listHeader="Hot Deals"></ItemList>
+          <MostBoughtSlider listHeader="Most Boughts on RentWise!"></MostBoughtSlider>
         </div>
+      </div> */}
       </div>
-    </div>
+    </>
   );
 }
